dacd26aa56210eaeb0ea3333e6aa298519d07f43
sungsik.kim <skystar819@gmail.com>
2024-07-17 07:36:39 +0900

    톰켓서버 추가

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -1,10 +1,14 @@
 package tobyspring.helloboot;
 
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+
 public class HellobootApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Containerless standalone Application");
-
+		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+		WebServer webServer = serverFactory.getWebServer();
+		webServer.start();
 	}
-
 }
