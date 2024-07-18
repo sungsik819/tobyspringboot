5ee51b07ea36b26082212663061ec138679f3051
sungsik.kim <skystar819@gmail.com>
2024-07-18 11:32:54 +0900

    jdbc 설정 적용 및 hikari 적용

---
+++ src/main/java/tobyspring/config/autoconfig/MyDataSourceProperties.java
@@ -0,0 +1,43 @@
+package tobyspring.config.autoconfig;
+
+import tobyspring.config.MyConfigurationProperties;
+
+@MyConfigurationProperties(prefix = "data")
+public class MyDataSourceProperties {
+    private String driverClassName;
+    private String url;
+    private String username;
+    private String password;
+
+    public void setDriverClassName(String driverClassName) {
+        this.driverClassName = driverClassName;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getDriverClassName() {
+        return driverClassName;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
